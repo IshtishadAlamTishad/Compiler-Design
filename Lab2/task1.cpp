b9f5cfe2364732194184231d0bf880157105b518
Tishad <96460346+IshtishadAlamTishad@users.noreply.github.com>
2024-02-10 23:45:14 +0600

    Update task1.cpp

--- Lab2/task1.cpp
+++ Lab2/task1.cpp
@@ -21,7 +21,7 @@ int main()
 
     bool ok = false;
     for(int i=0;i<s.size();++i) {
-        if((s[i] >= 97 && s[i] <= 122) || (s[i] >=65 && s[i] <=90)) {
+        if((s[i] >= ra && s[i] <= rz) || (s[i] >=rA && s[i] <=rZ)) {
             ok = true;
         }
     }
